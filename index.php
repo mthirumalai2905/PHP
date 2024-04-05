7f4622b92530e0511e023274c75c20bc355d3471
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-04-06 01:35:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
-<?php
-    echo "Hello, World!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        This is my first php website
+        <?php
+         echo "This is printed using php and its a dynamically typed language";
+
+         // single line comment
+         /*
+         This is a multi line comment
+         
+         */
+
+        $variable1 = 34;
+        $variable2 = 43;
+        echo $variable1;
+        echo $variable2;
+
+        echo $variable1 + $variable2;
+
+        ?>
+        <?php
+        
+        echo "Hello world again"
+        ?>
+    </div>
+</body>
+</html>
