6ebf1bc12e961806c9fc7d10299111f756f267c5
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-04-06 10:39:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,72 @@
         ?>
         <?php
         
-        echo "Hello world again"
+        echo "Hello world again";
+
+        // Operators in PHP
+
+        //Aritmetic Operators
+        echo "The value of variable + variable2";
+        echo "<br>"; // New Line 
+        echo $variable1 + $variable2;
+        echo "<br>";
+        echo "The value of variable - variable2";
+        echo "<br>"; // New Line 
+        echo $variable1 - $variable2;
+        echo "<br>";
+        echo "The value of variable * variable2";
+        echo "<br>"; // New Line 
+        echo $variable1 * $variable2;
+        echo "<br>";
+
+        //Assignment Operators
+        $newVar  = $variable1;
+        echo "The value of new varible is ";
+        echo $newVar;
+        echo "<br>";
+
+        //Increment/Decrement Operators
+        $variable1++;
+        echo $variable1;
+        echo "<br>";
+        $variable1--;
+        echo $variable1;
+        echo "<br>";
+        --$variable1;
+        echo $variable1;
+        echo "<br>";
+        ++$variable1;
+        echo $variable1;
+        echo "<br>";
+
+
+        //Logical Operator
+        // and (&&)
+        // or (||)
+        // xor ( ^ )
+        // not ( ! )
+        $myVar = (true and true);
+        echo var_dump($myVar);
+        echo "<br>";
+
+
+        //Comparison Operators
+        echo "<h1>Comparison Operators</h1>";
+        echo "The value of 1 == 4";
+        echo var_dump(1==4);
+        echo "<br>";
+        echo "The value of 1 >= 4";
+        echo var_dump(1>=4);
+        echo "<br>";
+        echo "The value of 1 <= 4";
+        echo var_dump(1<=4);
+        echo "<br>";
+        echo "The value of 1 != 4";
+        echo var_dump(1!=4);
+
+
+
+
         ?>
     </div>
 </body>
